e9bc50b67b67dc5cd072e8be79d3535e715a0a34
NguyenLeHongTham1402 <1951052184tham@ou.edu.vn>
2023-03-08 15:53:04 +0700

    Second Push

--- 3SLoginPage/Repository/LoginRepo.cs
+++ 3SLoginPage/Repository/LoginRepo.cs
@@ -1,7 +1,9 @@
 ﻿using _3SLoginPage.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,17 +12,22 @@ namespace _3SLoginPage.Repository
     public class LoginRepo
     {
         private ConfigurationContext context;
+        
+        //Trả về "t" là đăng nhập thành công
+        //Trả về "f" là đăng nhập không thành công có thể sai tham số truyền vào hoặc đã hết hạn hoạt động
+        //Còn lại là trả về chuỗi message của ngoại lệ
         public string isLogin (IFormCollection collection)
         {
             string status = string.Empty;
-            TbUser user = new TbUser();
-            user.UserId = collection["UserId"].ToString();
-            user.Password = collection["Password"].ToString();
+
+            string user_name = collection["UserId"].ToString();
+            string pass = collection["Password"].ToString();
             using (context  = new ConfigurationContext())
             {
                 try
                 {
-                    var u = context.TbUsers.FirstOrDefault(x => x.UserId == user.UserId && x.Password == user.Password && x.IsActive == true);
+                    //var u = context.TbUsers.SingleOrDefault(x => x.UserId == user_name && x.Password == pass && x.IsActive == true);
+                    var u = context.TbUsers.AsEnumerable().SingleOrDefault(x => x.UserId.Contains(user_name) && x.Password.Contains(pass) && x.IsActive == true);
                     if (u != null)
                         status = "t";
                     else
